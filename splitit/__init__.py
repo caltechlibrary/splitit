667294c8c9426e500e6b3f1a8fb7b5088cd57919
Michael Hucka <mhucka@caltech.edu>
2019-08-07 14:04:20 -0700

    Add some common files I used in all projects

---
+++ splitit/__init__.py
@@ -0,0 +1,7 @@
+'''
+Split It!
+'''
+
+from .__version__ import __version__, __title__, __description__, __url__
+from .__version__ import __author__, __email__
+from .__version__ import __license__, __copyright__
---
+++ splitit/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Split It! version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/splitit
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'Split It!'
+__description__ = 'Reformat certain spreadsheets of results downloaded by Caltech Library staff from caltech.tind.io when they are doing inventory.'
+__url__         = 'https://github.com/caltechlibrary/splitit'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'helpdesk@library.caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2019 by the California Institute of Technology'
